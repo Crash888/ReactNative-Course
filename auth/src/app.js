53f5d35f7b9f063e85f35d02c5e694bf03c456c5
dd <ddresearch@shaw.ca>
2017-08-04 16:06:17 -0700

    Start Firebase connection

--- auth/src/app.js
+++ auth/src/app.js
@@ -9,18 +9,6 @@ import LoginForm from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
-		/*
-		firebase.initializeApp( {
-    		apiKey: 'AIzaSyBCe8ci2padQtXJnk4rJp_Y-EvNQmBUyE4',
-    		authDomain: 'authentication-73969.firebaseapp.com',
-    		databaseURL: 'https://authentication-73969.firebaseio.com',
-    		projectId: 'authentication-73969',
-    		storageBucket: 'authentication-73969.appspot.com',
-    		messagingSenderId: '241448031156'
-  		})
-  		*/
-  		console.log("Firebase keys: " + FirebaseKeys);
-
   		firebase.initializeApp ( 
   			FirebaseKeys
   		)
