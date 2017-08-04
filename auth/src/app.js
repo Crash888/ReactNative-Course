4c2e7302384e10ef0e446548c72fdc183b9a8c7d
dd <ddresearch@shaw.ca>
2017-08-04 15:32:17 -0700

    Form Styling

---
+++ auth/src/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp( {
+    		apiKey: 'AIzaSyBCe8ci2padQtXJnk4rJp_Y-EvNQmBUyE4',
+    		authDomain: 'authentication-73969.firebaseapp.com',
+    		databaseURL: 'https://authentication-73969.firebaseio.com',
+    		projectId: 'authentication-73969',
+    		storageBucket: 'authentication-73969.appspot.com',
+    		messagingSenderId: '241448031156'
+  		})
+	}
+
+	render() {
+		return (
+			<View>
+				<Header headerText="Authentication"/>
+				<LoginForm />
+			</View>
+		)
+	}
+}
+
+export default App;
